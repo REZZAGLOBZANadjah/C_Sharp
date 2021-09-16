86a1bf521f847ec02f946c5afa366487af29e331
REZZAG LOBZA Nadjah <rezzag39dz@gmail.com>
2021-09-16 11:26:44 +0100

    Add files via upload

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Rezzag Lobza";
+            string secondName = "Nadjah";
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Rezzag Lobza")
+            .Append(" Nadjah")
+            .Append(" Nadjah")
+            .Append(" Nadjah")
+            .Append(" Nadjah");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}

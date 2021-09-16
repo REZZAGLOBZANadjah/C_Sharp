8ac6a35386e044ca41e0980ee3d11241d2afe07b
REZZAG LOBZA Nadjah <rezzag39dz@gmail.com>
2021-09-16 12:55:18 +0100

    Add files via upload

---
+++ WpfAppEmployeesDatabase/WpfAppEmployeesDatabase/DBEmployee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace MySqlCRUD
+{
+
+    class DBEmployee
+    {
+        public static MySqlConnection GetConnection()
+        {
+            string sql = @"server = localhost; user id = root;;password=25101991; database = company";
+            MySqlConnection conn = new MySqlConnection(sql);
+            try
+            {
+                conn.Open();
+
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show("Mysql connection erreur"+e.Message+"\n Erreur"+MessageBoxButtons.OK+ MessageBoxIcon.Erreur);
+                Console.WriteLine("Error: {0}", e.ToString());
+            }
+          
+            return conn;
+        }
+
+
+
+        public List<Employee> GetAll()
+        {
+            var employees = new List<Employee>();
+
+            string sql = @"server = localhost; user id = root;;password=25101991; database = company";
+            MySqlConnection conn = new MySqlConnection(sql);
+             conn.Open();
+            string query = "SELECT * FROM employee;";
+
+            MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds, "employee");
+            DataTable dt = ds.Tables["employee"];
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (DataColumn col in dt.Columns)
+                {
+                    Console.Write(row[col] + "\t");
+                }
+
+                Console.Write("\n");
+            }
+
+        }
+    }
---
+++ WpfAppEmployeesDatabase/WpfAppEmployeesDatabase/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+namespace MySqlCRUD
+{
+
+    public class Employee
+    {
+        public int empNo { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public double salary { get; set; }
+        public string dep { get; set; }
+
+    }
+}
